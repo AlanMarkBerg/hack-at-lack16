6abc4c56878934af1b64d8982f2cc0da57d4eba6
AlanMarkBerg <alan.berg.phd@gmail.com>
2016-04-18 21:29:46 +0200

    Add R exampe to get up to date descriptions of activity type Example only

---
+++ scripts/activity_types.R
@@ -0,0 +1,16 @@
+# Get names of verbs and display.
+# Save to a CSV file
+# Alan Berg
+
+library(httr)
+library(jsonlite)
+url_adl<-"https://w3id.org/xapi/adl"
+req_adl <- GET(url_adl, add_headers("Accept" = "application/ld+json" ))
+json_adl <- content(req_adl, as = "text")
+info_adl <- fromJSON(json_adl)
+# Names of activities and descriptions
+info_adl_table<-cbind(info_adl$`activity-types`$prefLabel,info_adl$`activity-types`$definition)
+cat("Verbs")
+info_adl_table[,1]
+colnames(info_adl_table)<-c("ActivityType","Description")
+write.table(info_adl_table, file = "verbs.csv", sep = ",", row.names = FALSE)
